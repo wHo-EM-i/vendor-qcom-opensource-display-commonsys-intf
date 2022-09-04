1ebc3b7c336ad5e7c11ca9ce1130032aa3928151
wHo-EM-i <ehteshammalik18998@gmail.com>
2022-09-05 01:26:12 +0530

    Revert "vendor-freeze: Move few commonsys-intf/display libs to commonsys/display"
    This reverts commit 7edc05ecad34c42cd16859bc6b5d0bd9513ac745.

--- config/display-product-system.mk
+++ config/display-product-system.mk
@@ -1,4 +1,9 @@
-PRODUCT_PACKAGES += vendor.display.config@1.0 \
+PRODUCT_PACKAGES += libdisplayconfig \
+                    libqdMetaData \
+                    libqdMetaData.system \
+                    libgralloc.qti \
+                    libdrm \
+                    vendor.display.config@1.0 \
                     vendor.display.config@1.1 \
                     vendor.display.config@1.2 \
                     vendor.display.config@1.3 \
@@ -11,8 +16,15 @@ PRODUCT_PACKAGES += vendor.display.config@1.0 \
                     vendor.qti.hardware.display.config-V5-ndk \
                     vendor.qti.hardware.display.config-V6-ndk
 
+SOONG_CONFIG_NAMESPACES += qtidisplaycommonsys
 SOONG_CONFIG_NAMESPACES += qtiunifeddraw
 # Soong Keys
+SOONG_CONFIG_qtidisplaycommonsys := displayconfig_enabled
 SOONG_CONFIG_qtiunifeddraw := qtiunifeddraw_enabled
 # Soong Values
+SOONG_CONFIG_qtidisplaycommonsys_displayconfig_enabled := false
 SOONG_CONFIG_qtiunifeddraw_qtiunifeddraw_enabled := true
+
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+    SOONG_CONFIG_qtidisplaycommonsys_displayconfig_enabled := true
+endif
